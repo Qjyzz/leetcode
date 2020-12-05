0430efc5445d1670c61f6578306f67155c25784d
Qjyzz <616200374@qq.com>
2020-12-06 00:00:13 +0800

    upgrade

--- topic/1. 两数之和.cpp
+++ topic/1. 两数之和.cpp
@@ -2,7 +2,6 @@
 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- 
 
 示例:
 
@@ -11,6 +10,8 @@
 因为 nums[0] + nums[1] = 2 + 7 = 9
 所以返回 [0, 1]
 */
+
+// 遍历两次，由于题目要求同一个元素不能使用两次，
 
 class Solution {
 public:
@@ -27,5 +28,21 @@ public:
             }
         }
         return result;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); ++i) {
+                auto it = mp.find(target - nums[i]);
+                if(it != mp.end()) {
+                    return {it->second, i};
+                }
+                mp[nums[i]]=i;
+        }
+        return {};
     }
 };
---
+++ topic/2. 两数相加.cpp
@@ -0,0 +1,30 @@
+/*难度中等5346收藏分享切换为英文接收动态反馈给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (nums[i] + nums[j] == target)
+                {
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+};
